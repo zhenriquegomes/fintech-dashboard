c5943a989ae1578e999223d23ebbde5dd2bcb695
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-06-10 08:04:48 -0300

    feat: add sales context provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { SalesContextProvider } from "./context/SalesContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<SalesContextProvider>
+			<App />
+		</SalesContextProvider>
 	</React.StrictMode>
 );
