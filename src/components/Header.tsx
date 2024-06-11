4136c4205761526de4f3d808884be4ec35c3f80d
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-06-11 06:48:02 -0300

    feat: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
+import { Stack } from "@mui/material";
+import DateRange from "./DataRange";
+import Months from "./Months";
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<Stack spacing={2} justifyContent="center">
+			<DateRange />
+			<Months />
+		</Stack>
+	);
 };
 
 export default Header;
