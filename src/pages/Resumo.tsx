d09ccbf140424349ae76375f25bb466a8a074ef9
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-06-12 06:40:41 -0300

    feat: create resume

--- src/pages/Resumo.tsx
+++ src/pages/Resumo.tsx
@@ -1,5 +1,33 @@
+import { Stack } from "@mui/material";
+import useSales from "../hooks/useSales";
+import SaleResume from "../components/SaleResume";
+
 const Resumo = () => {
-	return <div>Resumo</div>;
+	const { data } = useSales();
+	if (!data) return null;
+	data.forEach((sale) => console.log(sale.preco));
+	return (
+		<Stack direction="row" spacing={2}>
+			<SaleResume
+				name="Sales"
+				value={data
+					.filter((sale) => sale.status != "falha")
+					.reduce((sum, sale) => sum + sale.preco, 0)}
+			/>
+			<SaleResume
+				name="Received"
+				value={data
+					.filter((sale) => sale.status === "pago")
+					.reduce((sum, sale) => sum + sale.preco, 0)}
+			/>
+			<SaleResume
+				name="Processing"
+				value={data
+					.filter((sale) => sale.status === "processando")
+					.reduce((sum, sale) => sum + sale.preco, 0)}
+			/>
+		</Stack>
+	);
 };
 
 export default Resumo;
