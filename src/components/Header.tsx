b56df1327e8fb7ef8291b07e5c570b95feb007a5
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-06-19 07:00:32 -0300

    feat: change title with the location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
 import { Stack } from "@mui/material";
 import DateRange from "./DataRange";
 import Months from "./Months";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+	const [title, setTitle] = useState("Dashboard | Summary");
+	const location = useLocation();
+	useEffect(() => {
+		if (location.pathname === "/") {
+			setTitle("Dashboard | Summary");
+			document.title = title;
+		} else if (location.pathname === "/sales") {
+			setTitle("Dashboard | Sales");
+			document.title = title;
+		}
+	}, [location, title]);
 	return (
 		<Stack spacing={2} justifyContent="center" marginBottom={2}>
 			<DateRange />
