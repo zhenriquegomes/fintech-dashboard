4a892046ad03e1a441136b6f725fc36d767ea4c8
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-06-19 07:27:40 -0300

    fix: remove debug console.log

--- src/components/Months.tsx
+++ src/components/Months.tsx
@@ -19,7 +19,6 @@ const Months = () => {
 		date.setMonth(date.getMonth() + n);
 		const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
 		const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-		console.log(firstDay, lastDay);
 		setInitialDate(formatDate(firstDay));
 		setFinalDate(formatDate(lastDay));
 	}
